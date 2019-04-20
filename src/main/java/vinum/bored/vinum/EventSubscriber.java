c5225a8b8c667e3c92bed0a6cfddfdfc243be964
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2019-04-20 15:36:30 +1000

    Update EventSubscriber.java

--- src/main/java/vinum/bored/vinum/EventSubscriber.java
+++ src/main/java/vinum/bored/vinum/EventSubscriber.java
@@ -1,72 +1,132 @@
 package vinum.bored.vinum;
 
 import vinum.bored.vinum.Vinum;
-import vinum.bored.vinum.item.BasicItem;
 
 import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent.Register;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import com.google.common.base.Preconditions;
+import javax.annotation.Nonnull;
 
 @EventBusSubscriber(modid = Vinum.MODID)
 public final class EventSubscriber {
 
+	/**
+	 * This method will be called by Forge when it is time for the mod to register its blocks.
+	 * This method will always be called before the item registry method.
+	 */
+	@SubscribeEvent
+	public static void onRegisterBlocksEvent(@Nonnull final RegistryEvent.Register<Block> event) {
+
+		event.getRegistry().registerAll(
+			setup(new Block(), "test_block")
+		);
+
+	}
+
 	@SubscribeEvent
 	public static void registerItems(Register<Item> event) {
 
-		final Item[] items = {
+		final IForgeRegistry<Item> registry = event.getRegistry();
 
-				new BasicItem().setRegistryName("test_item").setUnlocalizedName(Vinum.MODID + "." + "test_item"),
-				//Wine Bottles
-				new BasicItem().setRegistryName("cork_bark").setUnlocalizedName(Vinum.MODID + "." + "cork_bark"),
-				new BasicItem().setRegistryName("wine_cork").setUnlocalizedName(Vinum.MODID + "." + "wine_cork"),
-				new BasicItem().setRegistryName("wine_press").setUnlocalizedName(Vinum.MODID + "." + "wine_press"),
-				new BasicItem().setRegistryName("wine_yeast").setUnlocalizedName(Vinum.MODID + "." + "wine_yeast"),
-				new BasicItem().setRegistryName("lead_ingot").setUnlocalizedName(Vinum.MODID + "." + "lead_ingot"),
-				new BasicItem().setRegistryName("lead_powder").setUnlocalizedName(Vinum.MODID + "." + "lead_powder"),
-				new BasicItem().setRegistryName("iron_powder").setUnlocalizedName(Vinum.MODID + "." + "iron_powder"),
-				//Wine Bottles
-				new BasicItem().setRegistryName("wine_bottle_empty").setUnlocalizedName(Vinum.MODID + "." + "wine_bottle_empty"),
-				new BasicItem().setRegistryName("wine_bottle_chardonnay").setUnlocalizedName(Vinum.MODID + "." + "wine_bottle_chardonnay"),
-				new BasicItem().setRegistryName("wine_bottle_sauvignon_blanc").setUnlocalizedName(Vinum.MODID + "." + "wine_bottle_sauvignon_blanc"),
-				new BasicItem().setRegistryName("wine_bottle_cabernet_sauvignon").setUnlocalizedName(Vinum.MODID + "." + "wine_bottle_cabernet_sauvignon"),
-				new BasicItem().setRegistryName("wine_bottle_merlot").setUnlocalizedName(Vinum.MODID + "." + "wine_bottle_merlot"),
-				new BasicItem().setRegistryName("wine_bottle_pinot_noir").setUnlocalizedName(Vinum.MODID + "." + "wine_bottle_pinot_noir"),
-				new BasicItem().setRegistryName("wine_bottle_zinfandel").setUnlocalizedName(Vinum.MODID + "." + "wine_bottle_zinfandel"),
-				//Wine Glasses
-				new BasicItem().setRegistryName("wine_glass_empty").setUnlocalizedName(Vinum.MODID + "." + "wine_glass_empty"),
-				new BasicItem().setRegistryName("wine_glass_chardonnay").setUnlocalizedName(Vinum.MODID + "." + "wine_glass_chardonnay"),
-				new BasicItem().setRegistryName("wine_glass_sauvignon_blanc").setUnlocalizedName(Vinum.MODID + "." + "wine_glass_sauvignon_blanc"),
-				new BasicItem().setRegistryName("wine_glass_cabernet_sauvignon").setUnlocalizedName(Vinum.MODID + "." + "wine_glass_cabernet_sauvignon"),
-				new BasicItem().setRegistryName("wine_glass_merlot").setUnlocalizedName(Vinum.MODID + "." + "wine_glass_merlot"),
-				new BasicItem().setRegistryName("wine_glass_pinot_noir").setUnlocalizedName(Vinum.MODID + "." + "wine_glass_pinot_noir"),
-				new BasicItem().setRegistryName("wine_glass_zinfandel").setUnlocalizedName(Vinum.MODID + "." + "wine_glass_zinfandel"),
-				//Grapes
-				new BasicItem().setRegistryName("grapes_chardonnay").setUnlocalizedName(Vinum.MODID + "." + "grapes_chardonnay"),
-				new BasicItem().setRegistryName("grapes_sauvignon_blanc").setUnlocalizedName(Vinum.MODID + "." + "grapes_sauvignon_blanc"),
-				new BasicItem().setRegistryName("grapes_cabernet_sauvignon").setUnlocalizedName(Vinum.MODID + "." + "grapes_cabernet_sauvignon"),
-				new BasicItem().setRegistryName("grapes_merlot").setUnlocalizedName(Vinum.MODID + "." + "grapes_merlot"),
-				new BasicItem().setRegistryName("grapes_pinot_noir").setUnlocalizedName(Vinum.MODID + "." +"grapes_pinot_noir"),
-				new BasicItem().setRegistryName("grapes_zinfandel").setUnlocalizedName(Vinum.MODID + "." + "grapes_zinfandel"),
-				//Grape Juice
-				new BasicItem().setRegistryName("grape_juice_chardonnay").setUnlocalizedName(Vinum.MODID + "." + "grape_juice_chardonnay"),
-				new BasicItem().setRegistryName("grape_juice_sauvignon_blanc").setUnlocalizedName(Vinum.MODID + "." + "grape_juice_sauvignon_blanc"),
-				new BasicItem().setRegistryName("grape_juice_cabernet_sauvignon").setUnlocalizedName(Vinum.MODID + "." + "grape_juice_cabernet_sauvignon"),
-				new BasicItem().setRegistryName("grape_juice_merlot").setUnlocalizedName(Vinum.MODID + "." + "grape_juice_merlot"),
-				new BasicItem().setRegistryName("grape_juice_pinot_noir").setUnlocalizedName(Vinum.MODID + "." + "grape_juice_pinot_noir"),
-				new BasicItem().setRegistryName("grape_juice_zinfandel").setUnlocalizedName(Vinum.MODID + "." + "grape_juice_zinfandel"),
-				//Grape Seeds
-				new BasicItem().setRegistryName("grape_seeds_chardonnay").setUnlocalizedName(Vinum.MODID + "." + "grape_seeds_chardonnay"),
-				new BasicItem().setRegistryName("grape_seeds_sauvignon_blanc").setUnlocalizedName(Vinum.MODID + "." + "grape_seeds_sauvignon_blanc"),
-				new BasicItem().setRegistryName("grape_seeds_cabernet_sauvignon").setUnlocalizedName(Vinum.MODID + "." + "grape_seeds_cabernet_sauvignon"),
-				new BasicItem().setRegistryName("grape_seeds_merlot").setUnlocalizedName(Vinum.MODID + "." + "grape_seeds_merlot"),
-				new BasicItem().setRegistryName("grape_seeds_pinot_noir").setUnlocalizedName(Vinum.NAME + "." + "grape_seeds_pinot_noir"),
-				new BasicItem().setRegistryName("grape_seeds_zinfandel").setUnlocalizedName(Vinum.MODID + "." + "grape_seeds_zinfandel")
+		registry.registerAll(
+			setup(new Item(), "test_item"),
+			// Wine Bottles
+			setup(new Item(), "cork_bark"),
+			setup(new Item(), "wine_cork"),
+			setup(new Item(), "wine_press"),
+			setup(new Item(), "wine_yeast"),
+			setup(new Item(), "lead_ingot"),
+			setup(new Item(), "lead_powder"),
+			setup(new Item(), "iron_powder"),
+			// TODO: make each wine type an enum and then turn all the hardcoded wines registration into a for loop?
+			// Wine Bottles
+			setup(new Item(), "wine_bottle_empty"),
+			setup(new Item(), "wine_bottle_chardonnay"),
+			setup(new Item(), "wine_bottle_sauvignon_blanc"),
+			setup(new Item(), "wine_bottle_cabernet_sauvignon"),
+			setup(new Item(), "wine_bottle_merlot"),
+			setup(new Item(), "wine_bottle_pinot_noir"),
+			setup(new Item(), "wine_bottle_zinfandel"),
+			// Wine Glasses
+			setup(new Item(), "wine_glass_empty"),
+			setup(new Item(), "wine_glass_chardonnay"),
+			setup(new Item(), "wine_glass_sauvignon_blanc"),
+			setup(new Item(), "wine_glass_cabernet_sauvignon"),
+			setup(new Item(), "wine_glass_merlot"),
+			setup(new Item(), "wine_glass_pinot_noir"),
+			setup(new Item(), "wine_glass_zinfandel"),
+			// Grapes
+			setup(new Item(), "grapes_chardonnay"),
+			setup(new Item(), "grapes_sauvignon_blanc"),
+			setup(new Item(), "grapes_cabernet_sauvignon"),
+			setup(new Item(), "grapes_merlot"),
+			setup(new Item(), "grapes_pinot_noir"),
+			setup(new Item(), "grapes_zinfandel"),
+			// Grape Juice
+			setup(new Item(), "grape_juice_chardonnay"),
+			setup(new Item(), "grape_juice_sauvignon_blanc"),
+			setup(new Item(), "grape_juice_cabernet_sauvignon"),
+			setup(new Item(), "grape_juice_merlot"),
+			setup(new Item(), "grape_juice_pinot_noir"),
+			setup(new Item(), "grape_juice_zinfandel"),
+			// Grape Seeds
+			setup(new Item(), "grape_seeds_chardonnay"),
+			setup(new Item(), "grape_seeds_sauvignon_blanc"),
+			setup(new Item(), "grape_seeds_cabernet_sauvignon"),
+			setup(new Item(), "grape_seeds_merlot"),
+			setup(new Item(), "grape_seeds_pinot_noir"),
+			setup(new Item(), "grape_seeds_zinfandel")
+		);
 
-		};
+		ForgeRegistries.BLOCKS.getValues().stream()
+		.filter(block -> block.getRegistryName().getNamespace().equals(Vinum.MODID))
+		.filter(EventSubscriber::hasItemBlock)
+		.forEach(block -> {
+				registry.register(setup(new ItemBlock(block), block.getRegistryName()));
+		});
 
-		event.getRegistry().registerAll(items);
+	}
 
+	/**
+	 * Helper method to determine if a block has an ItemBlock
+	 */
+	private static boolean hasItemBlock(@Nonnull final Block block) {
+		return true; //change this to return false if the block doesn’t have an ItemBlock
+	}
+
+	/**
+	 * Helper method to correctly setup a registry entry.
+	 * Calls setRegistryName with the appropriate parameters
+	 * If applicable also calls setTranslationKey with the appropriate parameters
+	 * @return Returns the entry passed in
+	 */
+	@Nonnull
+	public static <T extends IForgeRegistryEntry> T setup(@Nonnull final T entry, @Nonnull final String name) {
+		return setup(entry, new ResourceLocation(MOD_ID, name));
+	}
+
+	/**
+	 * Helper method to correctly setup a registry entry.
+	 * Calls setRegistryName with the appropriate parameters
+	 * If applicable also calls setTranslationKey with the appropriate parameters
+	 * @return Returns the entry passed in
+	 */
+	@Nonnull
+	public static <T extends IForgeRegistryEntry> T setup(@N﻿onnull final T entry, @Nonnull final ResourceLocation registryName) {
+
+		Preconditions.checkNotNull(entry, "entry to setup must not be null!");
+		Preconditions.checkNotNull(registryName, "registryName to assign must not be null!");
+
+		entry.setRegistryName(registryName);
+		if (entry instanceof Block) {
+			((Block) entry).setTranslationKey(registryName.getDomain() + "." + registryName.getPath());
+		}
+		if (entry instanceof Item) {
+			((Item) entry).setTranslationKey(registryName.getDomain() + "." + registryName.getPath());
+		}
+		return entry;
 	}
 
 }
